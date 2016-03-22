f2d8c80e962992ab21e0bcda1ef38124fd13fa44
futureGu <gukui1989@hotmail.com>
2016-03-22 18:31:52 +0800

    修改directive

--- js/directives/test-directive.js
+++ js/directives/test-directive.js
@@ -10,4 +10,10 @@ define(['./module'], function (directives) {
             replace:true
         };
     }]);
+    directives.directive('bDirective',[function(){
+        return{
+            restrict:'EAC',
+            template:'<h3 class="alert-danger">你好！这是由指令生成未作替换的标签!</h3>'
+        }
+    }]);
 });
